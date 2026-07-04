d7934e48369c6c9178b3e2f3da44152370b3e5a1
SHIVAM SATYAM <shivamsatyam209@gmail.com>
2026-07-04 22:46:58 +0530

    refined gateway

--- gateway/src/constants/messages.js
+++ gateway/src/constants/messages.js
@@ -1,7 +1,12 @@
 export const MESSAGES = {
-  GATEWAY_SERVICE_HEALTHY: "Gateway Service is healthy",
-  GATEWAY_FUEL_SERVICE_UNAVAILABLE: "Fuel Service is unavailable",
-  GATEWAY_NEWS_SERVICE_UNAVAILABLE: "News Service is unavailable",
-  GATEWAY_ANALYTICS_SERVICE_UNAVAILABLE: "Analytics Service is unavailable",
-  GATEWAY_SERVER_START: "Gateway running on port"
+  SUCCESS: {
+    GATEWAY_SERVICE_HEALTHY: "Gateway Service is healthy",
+    GATEWAY_SERVER_START: "Gateway running on port",
+  },
+  ERROR: {
+    GATEWAY_FUEL_SERVICE_UNAVAILABLE: "Fuel Service is unavailable",
+    GATEWAY_NEWS_SERVICE_UNAVAILABLE: "News Service is unavailable",
+    GATEWAY_ANALYTICS_SERVICE_UNAVAILABLE: "Analytics Service is unavailable",
+    GATEWAY_PORT_ERROR: 'PORT is not defined in the environment variables'
+  },
 };
--- gateway/src/server.js
+++ gateway/src/server.js
@@ -1,11 +1,26 @@
 import dotenv from "dotenv";
-import { MESSAGES } from './constants/messages.js'
-dotenv.config();
-
+import { MESSAGES } from "./constants/messages.js";
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`${MESSAGES.GATEWAY_SERVER_START} ${PORT}`);
-});
+const PORT = process.env.PORT;
+
+if (!PORT) {
+  console.error(MESSAGES.ERROR.GATEWAY_PORT_ERROR);
+  process.exit(1);
+}
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`${MESSAGES.SUCCESS.GATEWAY_SERVER_START} ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+
+    process.exit(1);
+  }
+};
+
+startServer();
